1c1b193332bac7ed6d5666fe09a341df6d5c4a77
brandonlim99 <142380212+brandonlhk@users.noreply.github.com>
2023-11-05 02:11:37 +0800

    countdown redirect to login
    added new method & data in vue, also added v-show in the HTML portion

--- src/signup.js
+++ src/signup.js
@@ -16,16 +16,32 @@ const app = Vue.createApp({
           },
 
           error_msg: "",
-
-
+          success_msg: "",
+          countdown: 3
+          
       }
   },
   methods: {
 
     find_caption(wheelchair) {
       this.caption = this.captions[wheelchair];
+    },
+
+    startRedirectCountDown() {
+      //if the countdown is not 0, it will call itself 
+      if (this.countdown !== -1){
+        //recursive function!!!
+        setTimeout(()=> {
+          this.success_msg = `Account created successfully! Redirecting you in ${this.countdown}`
+          this.countdown--
+          this.startRedirectCountDown()
+        }, 1000)
+      }
+      else {
+        location.href='login.html'
+      }
     }
-  }
+  }, 
 })
 
 const signUp = app.mount('#signUp')
@@ -53,8 +69,7 @@ function AddUser(userId, username, email) {
   if (error) {
       console.log(error)
   } else {
-    alert("Sign up successful,redirecting to login page")
-    location.href='login.html'
+    signUp.startRedirectCountDown()
   }
   });
 }
@@ -78,6 +93,7 @@ SignUp.addEventListener('click',()=>{
     signUp.error_msg = errMsg;
     // ..
   });
+
 
 
 })
